65f62812919a94035029393a78da2c9ef6160f8a
Rafael Souza Alcantara <rafael.asantos@sptech.school>
2025-09-04 14:23:02 -0300

    Update BancoGrupo.sql
    Atualizei os campos das tabelas deixando mais especifico, Usando _(nome da tabela)

--- BancoGrupo.sql
+++ BancoGrupo.sql
@@ -3,71 +3,71 @@ USE iceblood;
 
 CREATE TABLE Empresa (
     id_empresa INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(150) NOT NULL,
-    cnpj CHAR(14) NOT NULL,
-    endereco VARCHAR(200) NOT NULL,
-    CEP VARCHAR(200) NOT NULL,
-    contato VARCHAR(15) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    senha VARCHAR(30) NOT NULL,
-    dt_cadastro DATETIME,
-    responsavel VARCHAR(30),
-	CONSTRAINT chk_email CHECK (email LIKE '%@%')
+    nome_empresa VARCHAR(150) NOT NULL,
+    cnpj_empresa CHAR(14) NOT NULL,
+    endereco_empresa VARCHAR(200) NOT NULL,
+    CEP_empresa VARCHAR(200) NOT NULL,
+    contato_empresa VARCHAR(15) NOT NULL,
+    email_empresa VARCHAR(50) UNIQUE NOT NULL,
+    senha_empresa VARCHAR(30) NOT NULL,
+    dt_cadastro_empresa DATETIME,
+    responsavel_empresa VARCHAR(30),
+	CONSTRAINT chk_email_empresa CHECK (email_empresa LIKE '%@%')
 );
 
 CREATE TABLE Usuario (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    senha VARCHAR(50) NOT NULL,
-    cargo VARCHAR(50) NOT NULL,
-    permissao VARCHAR(20) NOT NULL
-    CONSTRAINT chk_permissao CHECK (permissao IN ('Admin', 'Operador', 'Visualizador'))
-	CONSTRAINT chk_email CHECK (email LIKE '%@%')
+    nome_usuario VARCHAR(100) NOT NULL,
+    email_usuario VARCHAR(100) UNIQUE NOT NULL,
+    senha_usuario VARCHAR(50) NOT NULL,
+    cargo_usuario VARCHAR(50) NOT NULL,
+    permissao_usuario VARCHAR(20) NOT NULL
+    CONSTRAINT chk_permissao_usuario CHECK (permissao_usuario IN ('Admin', 'Operador', 'Visualizador'))
+	CONSTRAINT chk_email_usuario CHECK (email_usuario LIKE '%@%')
 );
 
 CREATE TABLE Arduino (
     id_arduino INT AUTO_INCREMENT PRIMARY KEY,
-    local_instalado VARCHAR(100) NOT NULL,
-    stats VARCHAR(20) NOT NULL,
-    id_empresa INT NOT NULL,
-    temperatura DECIMAL(5,2) NOT NULL,
-    dt_hora DATETIME NOT NULL,
-    CONSTRAINT chk_stats_arduino CHECK (stats IN ('Ativo', 'Inativo', 'Manutenção'))
+    local_instalado_arduino VARCHAR(100) NOT NULL,
+    stats_arduino VARCHAR(20) NOT NULL,
+    temperatura_arduino DECIMAL(5,2) NOT NULL,
+    dt_hora_arduino DATETIME NOT NULL,
+    CONSTRAINT chk_stats_arduino_arduino CHECK (stats_arduino IN ('Ativo', 'Inativo', 'Manutenção'))
 );
 
 CREATE TABLE Doador (
     id_doador INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf CHAR(11) UNIQUE NOT NULL,
-    tipo_sanguineo CHAR(3) NOT NULL,
-    dt_nascimento DATE NOT NULL,
-    contato VARCHAR(15) NOT NULL,
-    CONSTRAINT chk_tipo_sanguineo CHECK (tipo_sanguineo IN ('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'))
+    nome_doador VARCHAR(100) NOT NULL,
+    cpf_doador CHAR(11) UNIQUE NOT NULL,
+    tipo_sanguineo_doador CHAR(3) NOT NULL,
+    dt_nascimento_doador DATE NOT NULL,
+    contato_doador VARCHAR(15) NOT NULL,
+    CONSTRAINT chk_tipo_sanguineo_doador CHECK (tipo_sanguineo_doador IN ('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'))
 );
 
 CREATE TABLE Bolsa_Sangue (
     id_bolsa INT AUTO_INCREMENT PRIMARY KEY,
-    doador VARCHAR(50), 
-    tipo_sanguineo CHAR(3) NOT NULL,
-    validade DATE NOT NULL,
-    dt_doacao DATETIME,
-    quantidade_ml INT NOT NULL,
-    stats VARCHAR(20) NOT NULL,
-    CONSTRAINT chk_tipo_sanguineo_bolsa CHECK (tipo_sanguineo IN ('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')),
-	CONSTRAINT chk_stats_bolsa CHECK (stats IN ('Armazenada', 'Transfundida', 'Vencida'))
+    doador_bolsa VARCHAR(50), 
+    tipo_sanguineo_bolsa CHAR(3) NOT NULL,
+    validade_bolsa DATE NOT NULL,
+    dt_doacao_bolsa DATETIME,
+    quantidade_ml_bolsa INT NOT NULL,
+    stats_bolsa VARCHAR(20) NOT NULL,
+    CONSTRAINT chk_tipo_sanguineo_bolsa CHECK (tipo_sanguineo_bolsa IN ('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')),
+	CONSTRAINT chk_stats_bolsa CHECK (stats_bolsa IN ('Armazenada', 'Transfundida', 'Vencida'))
 );
 
 CREATE TABLE Alerta (
     id_alerta INT AUTO_INCREMENT PRIMARY KEY,
-    arduino INT NOT NULL, 
+    id_arduino INT NOT NULL, 
     tipo_alerta VARCHAR(30) NOT NULL,
-    statusGerador VARCHAR (30),
-    gravidadeAlerta VARCHAR (30),
-    dt_hora DATETIME NOT NULL,
-    statusAlerta VARCHAR(20) NOT NULL
-	CONSTRAINT chk_stats_alerta CHECK (statusAlerta IN ('Manunencao', 'Resolvido')),
+    statusGerador_alerta VARCHAR (30),
+    gravidadeAlerta_alerta VARCHAR (30),
+    dt_hora_alerta DATETIME NOT NULL,
+    status_alerta VARCHAR(20) NOT NULL
+	CONSTRAINT chk_stats_alerta CHECK (status_alerta IN ('Manunencao', 'Resolvido')),
 	CONSTRAINT chk_tipo_alerta CHECK (tipo_alerta IN ('Alta temperatura', 'Baixa temperatura', 'Falha sensor')),
-	CONSTRAINT chk_tipo_gravidadeAlerta CHECK (gravidadeAlerta IN ('Baixa', 'Media', 'Alta')),
-    CONSTRAINT chkGerador CHECK(statusGerador IN('Acionado','Suspenso'))
+	CONSTRAINT chk_tipo_gravidade_alerta CHECK (gravidade_alerta IN ('Baixa', 'Media', 'Alta')),
+    CONSTRAINT chk_Gerador_alerta CHECK(status_Gerador_alerta IN('Acionado','Suspenso'))
 );
+
